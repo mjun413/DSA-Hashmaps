3bf3be945648cb907c44179f58a003a273308e90
mjun413 <48771142+mjun413@users.noreply.github.com>
2019-11-21 14:52:12 -0500

    Create hashMap_drills.js

---
+++ hashMap_drills.js
@@ -0,0 +1,31 @@
+const HashMap = require('./HashMap');
+
+function main(items) {
+	const lor = new HashMap();
+	lor.MAX_LOAD_RATIO = 0.5;
+	lor.SIZE_RATIO = 3;
+	for (let i = 0; i < items.length; i++) {
+		lor.set(
+			Object.keys(items[i]).toString(),
+			Object.values(items[i]).toString()
+		);
+	}
+
+	console.log(lor);
+	console.log(lor.get('Maiar'));
+}
+
+const items = [
+	{ Hobbit: 'Bilbo' },
+	{ Hobbit: 'Frodo' },
+	{ Wizard: 'Gandolf' },
+	{ Human: 'Aragon' },
+	{ Elf: 'Legolas' },
+	{ Maiar: 'The Necromancer' },
+	{ Maiar: 'Sauron' },
+	{ RingBearer: 'Gollum' },
+	{ LadyOfLight: 'Galadriel' },
+	{ HalfElven: 'Arwen' },
+	{ Ent: 'Treebeard' }
+];
+console.log(main(items));
